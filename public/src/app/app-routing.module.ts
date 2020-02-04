4372d065263d62fe62da3ee74286e1672bbd6c4a
kyle-mcr <my email address used on my git repo>
2020-02-04 14:07:06 -0800

    added services page

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
 
 
-const routes: Routes = [{path:'',pathMatch:'full',component:HomeComponent}];
+const routes: Routes = [{path:'',pathMatch:'full',component:HomeComponent},
+{path:'services', component:ServicesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
