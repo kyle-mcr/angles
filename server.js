75f7637f250f856c2a63259484fd8b97bda0ad3e
kyle-mcr <my email address used on my git repo>
2020-02-05 16:35:32 -0800

    file changes

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express'),
-    path = require('path'),
     app = express()
 
 
@@ -11,11 +10,5 @@ const express = require('express'),
 //     'preflightContinue': false
 // }));
 app.use(express.static(__dirname + '/public/dist/public'));
-
-app.use(express.json());
-
-app.all("*", (req, res, next) => {
-    res.sendFile(path.resolve("./public//public/index.html"))
-});
 
 app.listen(8000, () => console.log('listening on port 8000'));
