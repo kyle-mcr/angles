73f85f6a450593d286e21ff6f7e99e0c7865b035
kyle-mcr <my email address used on my git repo>
2020-02-07 15:17:44 -0800

    retry

--- server.js
+++ server.js
@@ -1,23 +1,11 @@
-const express = require('express'),
-    path = require('path'),
-    request = require('request'),
-    app = express();
-    
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(express.static( __dirname + '/public/dist/public' ));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: true }));
 
 
-// app.use(cors({
-//     'allowedHeaders': ['sessionId', 'Content-Type'],
-//     'exposedHeaders': ['sessionId'],
-//     'origin': '*',
-//     'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     'preflightContinue': false
-// }));
-app.use(express.static(__dirname + '/public/dist/public'));
-
-app.use(express.json());
-
-app.all("*", (req, res, next) => {
-    res.sendFile(path.resolve("./public//public/index.html"))
-});
-
-app.listen(8000, () => console.log('listening on port 8000'));
+app.listen(8000, () => console.log("listening on port 8000"));
