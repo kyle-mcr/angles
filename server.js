3cc1a553b05e353f3ccd823dec29a89be953da8b
kyle-mcr <my email address used on my git repo>
2020-02-07 14:48:05 -0800

    final

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express'),
+    path = require('path'),
     app = express()
 
 
@@ -10,5 +11,11 @@ const express = require('express'),
 //     'preflightContinue': false
 // }));
 app.use(express.static(__dirname + '/public/dist/public'));
+
+app.use(express.json());
+
+app.all("*", (req, res, next) => {
+    res.sendFile(path.resolve("./public//public/index.html"))
+});
 
 app.listen(8000, () => console.log('listening on port 8000'));
